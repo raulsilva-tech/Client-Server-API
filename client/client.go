e78c09ee1fbf6bf03a1bc3a363e46f5d4a53d038
Raul Paes Silva <raulpaes.work@gmail.com>
2023-12-09 23:50:19 -0300

    Fist commit

---
+++ client/client.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Usdbrl struct {
+	Code       string `json:"code"`
+	Codein     string `json:"codein"`
+	Name       string `json:"name"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	VarBid     string `json:"varBid"`
+	PctChange  string `json:"pctChange"`
+	Bid        string `json:"bid"`
+	Ask        string `json:"ask"`
+	Timestamp  string `json:"timestamp"`
+	CreateDate string `json:"create_date"`
+}
+
+type CotacaoAPI struct {
+	Usdbrl `json:"USDBRL"`
+}
+
+func main() {
+
+	//criando o contexto com def de timeout de 300ms
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	defer cancel()
+	//se timeout da requisição for excedido: registre log
+	select {
+	case <-ctx.Done():
+		log.Println("Request cancelada por timeout do contexto.")
+	default:
+	}
+
+	//criando requisição
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:8080/cotacao", nil)
+	if err != nil {
+		panic(err)
+	}
+	//executando requisição
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	if resp.StatusCode != 200 {
+		fmt.Println("StatusCode Error: ", resp.StatusCode)
+		return
+	}
+	defer resp.Body.Close()
+
+	//obtendo corpo da requisição
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	//exibindo retorno json da requisição
+	fmt.Println(string(bodyBytes))
+
+	//transformando corpo na struct Cotacao
+	var data CotacaoAPI
+	err = json.Unmarshal(bodyBytes, &data)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Não foi possivel converter o json recebido: %v\n", err)
+		panic(err)
+	}
+
+	//obtendo campo Bid
+	bid := data.Usdbrl.Bid
+
+	//registrando cotação no arquivo texto
+	err = logCotacao(bid)
+	if err == nil {
+		fmt.Println("Cotação registrada com sucesso no arquivo cotacao.txt")
+	}
+}
+
+func logCotacao(bid string) error {
+
+	file, err := os.Create("cotacao.txt")
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Não foi possivel criar o arquivo: %v\n", err)
+		return err
+	}
+	_, err = file.WriteString("Dólar: " + bid)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Não foi possivel criar o arquivo: %v\n", err)
+		return err
+	}
+	defer file.Close()
+	return nil
+}
---
+++ server/server.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Usdbrl struct {
+	Code       string `json:"code"`
+	Codein     string `json:"codein"`
+	Name       string `json:"name"`
+	High       string `json:"high"`
+	Low        string `json:"low"`
+	VarBid     string `json:"varBid"`
+	PctChange  string `json:"pctChange"`
+	Bid        string `json:"bid"`
+	Ask        string `json:"ask"`
+	Timestamp  string `json:"timestamp"`
+	CreateDate string `json:"create_date"`
+}
+
+type CotacaoAPI struct {
+	Usdbrl `json:"USDBRL"`
+}
+
+type CotacaoDB struct {
+	ID         int `gorm:"primaryKey"`
+	CotacaoAPI `gorm:"CotacaoAPI"`
+}
+
+func main() {
+
+	fmt.Println("Iniciado ")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/cotacao", CotacaoHandler)
+	http.ListenAndServe(":8080", mux)
+
+}
+
+func CotacaoHandler(w http.ResponseWriter, r *http.Request) {
+
+	data, err := GetCotacao()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	//criando header do tipo json
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	//retornando json na resposta da requisição
+	json.NewEncoder(w).Encode(data)
+
+}
+
+func GetCotacao() (*CotacaoAPI, error) {
+
+	//criando o contexto com def de timeout de 200ms
+	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	defer cancel()
+
+	//criando requisição
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
+	if err != nil {
+		panic(err)
+	}
+	//executando requisição
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao fazer requisição: %v\n", err)
+		//se timeout excedido: registrar log e devolver erro
+		select {
+		case <-ctx.Done():
+			log.Println("Request cancelada por timeout do contexto da requisição de https://economia.awesomeapi.com.br/json/last/USD-BRL")
+		default:
+		}
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		err = errors.New("Erro " + fmt.Sprint(resp.StatusCode))
+		return nil, err
+	}
+
+	//obtendo corpo da requisição
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Erro ao ler body: %v\n", err)
+		return nil, err
+	}
+
+	//transformando corpo na struct CotacaoAPI
+	var data CotacaoAPI
+	err = json.Unmarshal(bodyBytes, &data)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Não foi possivel converter o json recebido: %v\n", err)
+		return nil, err
+	}
+
+	//registrando cotação no banco de dados
+	_ = saveData(data)
+
+	return &data, err
+
+}
+
+func saveData(data CotacaoAPI) error {
+
+	//fazendo conexão com banco de dados
+	db, err := gorm.Open(sqlite.Open("cotacoes.db"), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	//criando tabela
+	db.AutoMigrate(&CotacaoDB{})
+
+	//contexto para definir timeout de 10ms
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Millisecond*10)
+	defer cancel()
+
+	//inserindo nova cotação
+	db.WithContext(ctx).Create(&CotacaoDB{
+		CotacaoAPI: CotacaoAPI{
+			Usdbrl: Usdbrl{
+				Code:       data.Usdbrl.Code,
+				Codein:     data.Usdbrl.Codein,
+				Name:       data.Usdbrl.Name,
+				High:       data.Usdbrl.High,
+				Low:        data.Usdbrl.Low,
+				VarBid:     data.Usdbrl.VarBid,
+				PctChange:  data.Usdbrl.PctChange,
+				Bid:        data.Usdbrl.Bid,
+				Ask:        data.Usdbrl.Ask,
+				Timestamp:  data.Usdbrl.Timestamp,
+				CreateDate: data.Usdbrl.CreateDate,
+			},
+		}})
+
+	return nil
+}
